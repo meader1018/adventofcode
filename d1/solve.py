2883de1671ddf19dbb1562d9441be249f4024bd2
meader1018 <meader@uci.edu>
2020-12-19 04:00:54 -0800

    day 1 full solution

--- d1/solve.py
+++ d1/solve.py
@@ -2,9 +2,18 @@
 
 with open("input.txt", 'r') as infile, open("output.txt", 'w') as outfile:
 	remainder = {}
+	part_two_found = False
 	for line in infile:
-		if remainder.get(2020-int(line), False):
-			outfile.write(str(int(line)*(2020-int(line))))
+		num = int(line)
+		if remainder.get(2020-num, False):
+			outfile.write("Part 1 Solution: " + str(num*(2020-num)) + "\n")
 		else:
-			remainder[int(line)] = int(line)
+			remainder[num] = num
+		if not part_two_found:	
+			for num2 in remainder:
+				#check part_two_found flag twice, once within for loop and once for the for loop to run
+				if remainder.get(2020-num-num2, False) and not part_two_found:
+					part_two_found = True
+					outfile.write("Part 2 Solution: " + str(num*num2*(2020-num-num2)) + "\n")
+
 
