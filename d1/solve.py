b14115c12db898754a43a99aace4f0c7f6216786
meader1018 <meader@uci.edu>
2020-12-19 18:39:46 -0800

    day 2 part 2 solution

--- d1/solve.py
+++ d1/solve.py
@@ -5,13 +5,14 @@ with open("input.txt", 'r') as infile, open("output.txt", 'w') as outfile:
 	part_two_found = False
 	for line in infile:
 		num = int(line)
-		if remainder.get(2020-num, False):
+		if remainder.get(2020-num, False): # part 1
 			outfile.write("Part 1 Solution: " + str(num*(2020-num)) + "\n")
 		else:
 			remainder[num] = num
-		if not part_two_found:	
+		if not part_two_found: # part 2
 			for num2 in remainder:
-				#check part_two_found flag twice, once within for loop and once for the for loop to run
+				# check part_two_found flag twice, once within for loop to prevent duplicate solutions
+				# and once to prevent for loop from running unnecessarily
 				if remainder.get(2020-num-num2, False) and not part_two_found:
 					part_two_found = True
 					outfile.write("Part 2 Solution: " + str(num*num2*(2020-num-num2)) + "\n")
--- d2/solve.py
+++ d2/solve.py
@@ -1,15 +1,26 @@
 # https://adventofcode.com/2020/day/2
 
 with open("input.txt", 'r') as infile, open("output.txt", 'w') as outfile:
-	valid_passwords = 0
-	broken_string = []
+	valid_passwords_1 = 0
+	valid_passwords_2 = 0
+	boundary_low, boundary_high = 0, 0
+	split_string = []
 	for line in infile:
-		broken_string = line.split(" ")
-		num_occurrences = broken_string[2].count(broken_string[1][0])
-		broken_string[0] = broken_string[0].replace("-", "")
-		size_range = len(broken_string[0])		
-		
-		# following conditional only works for upper_bound < 100
-		if int(broken_string[0][0:size_range//2]) <= num_occurrences <= int(broken_string[0][size_range//2:]):
-			valid_passwords += 1
-	outfile.write("Part 1 Solution: " + str(valid_passwords))
+		split_string = line.split(" ")
+		char = split_string[1][0]
+		num_occurrences = split_string[2].count(char)
+		split_string[0] = split_string[0].replace("-", "")
+		range_len = len(split_string[0])		
+		boundary_low = int(split_string[0][0:range_len//2])
+		boundary_high = int(split_string[0][range_len//2:])
+
+		# following conditionals only work for boundary_high < 100
+		# because of how boundaries are calculated
+		if boundary_low <= num_occurrences <= boundary_high:
+			valid_passwords_1 += 1
+			
+		if (split_string[2][boundary_low-1] == char) ^ (split_string[2][boundary_high-1] == char):
+			valid_passwords_2 += 1
+
+	outfile.write("Part 1 Solution: " + str(valid_passwords_1) + "\n")
+	outfile.write("part 2 Solution: " + str(valid_passwords_2))
